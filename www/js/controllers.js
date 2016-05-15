798f5d53c702a4358a243835e4c01b3e81a6d5f0
Eduardo Vital <vitaldu@gmail.com>
2016-05-15 16:12:01 -0300

    Added functionality of calculate journey

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,11 +41,75 @@ angular.module('starter.controllers', ['ngMap'])
   };
 })
 
-.controller('JourneyCtrl', function($scope, NgMap) {
+.controller('JourneyCtrl', function($scope, NgMap, $ionicPopup, $timeout) {
+
+  var vm = this;
+
   NgMap.getMap().then(function(map) {
-    console.log(map.getCenter());
-    console.log('markers', map.markers);
-    console.log('shapes', map.shapes);
+    vm.map = map;
+  });
+
+  $scope.disableTap = function() {
+    container = document.getElementsByClassName('pac-container');
+    angular.element(container).attr('data-tap-disabled', 'true');
+    angular.element(container).on("click", function() {
+      document.getElementById('searchBar').blur();
+    });
+  };
+
+  $scope.calculateJourney = function() {
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
+
+    origin = angular.element(document.getElementById('origin')).val();
+    destination = angular.element(document.getElementById('destination')).val();
+    waypoints = [];
+    waypoints.push({
+      location: angular.element(document.getElementsByClassName('waypoint')).val(),
+      stopover: true
+    });
+
+    directionsDisplay.setMap(vm.map);
+
+    directionsService.route({
+      origin: origin,
+      destination: destination,
+      waypoints: waypoints,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.BICYCLING
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+        var route = response.routes[0];
+        var summaryPanel = document.getElementById('directions-panel');
+        summaryPanel.innerHTML = '';
+        // For each route, display summary information.
+        for (var i = 0; i < route.legs.length; i++) {
+          var routeSegment = i + 1;
+          summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+            '</b><br>';
+          summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+          summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+          summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+        }
+    } else {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Directions request failed',
+        template: '<center>' + status + '</center>'
+      });
+    }
+  });
+  };
+
+  var vm = this;
+
+  vm.placeChanged = function() {
+    vm.place = this.getPlace();
+    console.info("mudou para" +  this.getPlace());
+  }
+
+  NgMap.getMap().then(function(map) {
+    vm.map = map;
   });
 })
 
